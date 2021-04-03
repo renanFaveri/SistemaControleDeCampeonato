467c383cf0db171f4d5110f65ad992b8ebbe41b3
Renan De Faveri Leal <renanfaveri01@gmail.com>
2021-04-03 18:53:05 -0300

    João - Entidade calendario (classe)

---
+++ SistemaControleDeCampeonato/MVC/entidade/calendario.py
@@ -0,0 +1,32 @@
+class Calendario:
+    
+    def __init__(self, data_inicial, data_final):
+        self.__data_inicial = data_inicial
+        self.__data_final = data_final
+        self.__calendario_jogos = None
+        
+    @property
+    def data_inicial(self):
+        return self.__data_inicial
+    
+    @data_inicial.setter
+    def data_inicial(self, data_inicial):
+        ### fazer conferencia
+        self.__data_inicial = data_inicial
+        
+    @property
+    def data_final(self):
+        return self.__data_final
+    
+    @data_final.setter
+    def data_final(self, data_final):
+        ### fazer conferencia
+        self.__data_final = data_final
+        
+    @property
+    def calendario(self):
+        return self.__calendario
+    
+    def definir_calendario(self, data_inicial, data_final):
+        pass
+    ########### implementar
