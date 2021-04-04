b9b7b80c748dfaaa20fa7157e06fa3fb334576f0
Renan De Faveri Leal <renanfaveri01@gmail.com>
2021-04-04 14:27:16 -0300

    João - Controle abstrato das entidades

---
+++ SistemaControleDeCampeonato/MVC/Controle/controleDeEntidade.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+
+class ControladorDeEntidade(ABC):
+    
+    @abstractmethod
+    def __init__(self):
+        self.__tela = None
+    
+    @property
+    @abstractmethod
+    def tela(self):
+        pass     
+
+    @tela.setter
+    @abstractmethod
+    def tela(self, tela):
+        pass
+        
+    @abstractmethod
+    def cadastrar(self):
+        pass
+    
+    @abstractmethod
+    def alterar(self):
+        pass
+    
+    @abstractmethod
+    def listar(self):
+        pass
+    
+    @abstractmethod
+    def excluir(self):
+        pass
+    
+    @abstractmethod
+    def mostrar_informacoes(self):
+        pass
+    
+    @abstractmethod
+    def abre_tela(self):
+        pass
+    
